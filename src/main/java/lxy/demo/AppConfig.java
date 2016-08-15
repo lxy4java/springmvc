2693fbd3faf89b337c341f1b8c02717469d2aafa
lixy <lixiaoyao@zszq.com>
2016-08-15 10:02:42 +0800

    @value

--- src/main/java/lxy/demo/AppConfig.java
+++ src/main/java/lxy/demo/AppConfig.java
@@ -2,15 +2,26 @@ package lxy.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import lxy.demo.hello.HelloAspect;
 
 @Configuration
+@PropertySource(value = "classpath:db.properties")
 public class AppConfig {
 
 	@Bean
 	HelloAspect helloAspect() {
 		return new HelloAspect();
+	}
+	
+	/***
+	 * 和  PropertySource 注解一起解析 properties 文件 并 可以被@value 读取
+	 * */
+	@Bean
+	public PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
 	}
 
 }
--- src/main/java/lxy/demo/hello/HelloWordController.java
+++ src/main/java/lxy/demo/hello/HelloWordController.java
@@ -1,16 +1,20 @@
 package lxy.demo.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
-	
+
+	@Value("${db1.jdbc.driver}")
+	private String driver;
+
 	@Hello(sayHello = "xxxx")
 	@RequestMapping("/hello")
 	public String hello() {
 
-		return "hello";
+		return "hello: " + driver;
 	}
 
 }
