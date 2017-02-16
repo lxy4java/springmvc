d8275d0338b8fc7e128939137d93bc29bfbd63a3
lixy <lixiaoyao@zszq.com>
2017-02-16 14:48:35 +0800

    定时服务。

--- src/main/java/lxy/demo/WebMVCConfiguration.java
+++ src/main/java/lxy/demo/WebMVCConfiguration.java
@@ -4,15 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import lxy.demo.hello.HelloInterceptor;
 
 @Configuration
 @ComponentScan(basePackages = { "lxy.demo" })
--- src/main/java/lxy/demo/job/HelloJob.java
+++ src/main/java/lxy/demo/job/HelloJob.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Component;
 public class HelloJob {
 
 	
-	@Scheduled(cron="0/5 * * * * ?")
+	@Scheduled(cron="0 11 14 ? * FRI")
+	//@Scheduled(cron="10 * * * * ?")
 	public void work(){
 		System.out.println("hello~~~~~~~~~~~~~`");
 	}
